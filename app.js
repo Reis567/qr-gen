6ac44058bfd65fecef7390473895fc22e08a2d06
Reis567 <matheusdosreis9@gmail.com>
2023-08-25 08:13:18 -0300

    animation

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ let qrText = document.getElementById('qrText');
 
 function generateQr(){
     qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+ qrText.value;
-
+    imgBox.classList.add("show-img");
 };

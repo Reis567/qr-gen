380ed80db0edda7fa085ddf445838de63b5ce638
Reis567 <matheusdosreis9@gmail.com>
2023-08-25 07:39:41 -0300

    js initial func and variables

---
+++ app.js
@@ -0,0 +1,7 @@
+let text = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${data}`;
+let imgBox = document.getElementById('imgBox');
+let qrImage = document.getElementById('qrImage');
+
+function generateQr(){
+
+}

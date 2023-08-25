a224996ef4f1186032a97bc695681b1bed5a74c0
Reis567 <matheusdosreis9@gmail.com>
2023-08-25 08:30:20 -0300

    input empty error

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ let imgBox = document.getElementById('imgBox');
 let qrImage = document.getElementById('qrImage');
 let qrText = document.getElementById('qrText');
 
-
-
-function generateQr(){
-    qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+ qrText.value;
-    imgBox.classList.add("show-img");
+function generateQr() {
+    if (qrText.value.length > 0) {
+        qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+        imgBox.classList.add("show-img");
+    } else {
+        qrText.classList.add('error');
+        qrText.style.borderColor = "var(--red)"; // Corrige o acesso à propriedade style
+        setTimeout(() => {
+            qrText.classList.remove('error');
+            qrText.style.borderColor = "var(--blue-hg)"; // Corrige o acesso à propriedade style
+        }, 1000);
+    }
 };
